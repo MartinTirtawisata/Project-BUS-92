75f4c4334e4e88c4f22ce35c7f7f138a88a553c7
SeanScud <seanscudellari@gmail.com>
2017-04-27 18:38:36 -0700

    Add files via upload

--- Clubs with SQL Database Module/my_app/source/views.py
+++ Clubs with SQL Database Module/my_app/source/views.py
@@ -32,25 +32,16 @@ Possible Ideas to make website better
 '''
 #SQL
 # Create a new reviews table? with  category_ID, Club_id, reviews
-#-------------------- Def function for printing Organizations---------
 
+#-------------Function for printing all Organizations (Main Table)---------
 
-def yesORno(boolean):
-    if (boolean == True):
-        return "Yes"
-    else:
-        return "No"
-        
-def print_table(SJSU_Organizations):
+def print_maintable(SJSU_Organizations):
     CLUB_ID = 0
     ORGANIZATION_NAME = 1
     PRESIDENT = 2
     LOCATION = 3
     CATEGORY = 4
     RATING = 5
-    NUMBER_OF_REVIEWS = 6
-    PAYMENT_REQUIRED = 7
-    MEMBERSHIP_COST = 8
 
 
     header = """
@@ -158,9 +149,6 @@ def print_table(SJSU_Organizations):
                 <th>Location</th>
                 <th>Category</th>
                 <th>Rating</th>
-                <th>Number Of Reviews</th>
-                <th>Payment Required</th>
-                <th>Membership Cost</th>
               </tr>
 
             </div>
@@ -179,13 +167,158 @@ def print_table(SJSU_Organizations):
                        '<td align="center">'+str(item[PRESIDENT])+'</td>'+\
                        '<td align="middle">'+str(item[LOCATION])+'</td>'+\
                        '<td align="middle">'+str(item[CATEGORY])+'</td>'+\
-                       '<td align="middle">'+str(item[RATING])+'</td>'+\
-                       '<td align="middle">'+str(item[NUMBER_OF_REVIEWS])+'</td>'+\
-                       '<td align="middle">'+yesORno((item[PAYMENT_REQUIRED]))+'</td>' +\
-                       '<td align="middle">'+str(item[MEMBERSHIP_COST]) + '</td>'+'</tr>'
+                       '<td align="middle">'+str(item[RATING])+'</td>'+'</tr>'
+
         key += 1
 
     return (header+message_out+footer)
+
+#----------Function for printing details of Organizations (Sub Table)---------
+
+def yesORno(boolean):
+    if (boolean == True):
+        return "Yes"
+    else:
+        return "No"
+
+
+def print_subtable(SJSU_Organizations):
+    CLUB_ID = 0
+    ORGANIZATION_NAME = 1
+    NUMBER_OF_MEMBERS = 2
+    NUMBER_OF_REVIEWS = 3
+    PAYMENT_REQUIRED = 4
+    MEMBERSHIP_COST = 5
+
+    header = """
+            <head>
+            <title>Further Organization Details</title>
+            <style>
+
+            body {
+                    margin: 0;
+                    padding: 0;
+            }
+            #fixedBar {
+                    height: 150px;
+                    color: #3072AD;
+                    background-color: #EAB010;
+                    margin: 0;
+                    text-align: center;
+            }
+            #logo {
+                    position: absolute;
+                    padding-top: 2px;
+                    overflow: auto;
+            }
+            #logo3 {
+
+                    height: 138px;
+            }
+
+            #home {
+                    padding-right: 25px;
+                    position: absolute;
+                    top: 10px;
+                    right: 1px;
+            }
+            #title {
+                    font-size: 340%;
+                    padding-top: 40px;
+                    font-family: "Times New Roman", Times, serif;
+            }
+            div.clear {
+                    float: clear;
+            }
+            #image {
+                background-image: url("http://www.mtmary.edu/_images/_main/interior-clubs-orgs-026.jpg");
+                height: 400px;
+                width: 100%;
+                margin: 0;
+                background-size:100% 100%;
+                background-repeat:no-repeat;
+            }
+            #info {
+                width: 100%;
+                height: 150px;
+                text-align: center;
+                background-color: #EAB010;
+                font-size: 110%;
+                margin: 0;
+                padding: 32px 0 0 0;
+            }
+            table, th, td {
+                border-style: outset;
+                border-collapse: collapse;
+                padding-left: 5px;
+                padding-right: 5px;
+                background-color: white;
+            }
+            th {
+                padding: 5px;
+                text-align: center;
+                color: blue;
+            }
+            </style>
+            </head>
+            <body>
+                <div id="fixedBar">
+
+                    <div id="logo">
+                        <img src="http://www.books-not-bombs.com/content/images/schools/sjsu.png" id="logo3" />
+                    </div>
+
+                    <div class="clear"> </div>
+
+
+                    <h1 id="title"><font face="Palatino Linotype"> Spartan Organizations</font></h1>
+
+                    <body link="yellow"><div id="home"><a href="http://127.0.0.1:5000/">Homepage</a></div></body>
+
+                <div id="image"> </div>
+
+
+                <div id="info">
+                    <p><font face="Georgia"><b>Here, you will find further details regarding each club</font></b></p>
+                </div>
+
+            <table style="width:100%">
+              <caption><h1>San Jose State Clubs and Organizations</h1></caption>
+
+              <tr>
+                <th>Club ID</th>
+                <th>Organization Name</th>
+                <th>Number Of Members</th>
+                <th>Number Of Reviews</th>
+                <th>Payment Required?</th>
+                <th>Membership Cost</th>
+              </tr>
+
+            </div>
+                """
+
+    footer = """
+    </table></body>
+    """
+    message_out = ''
+
+    key = 0
+    for item in SJSU_Organizations:
+        message_out += '<tr>' + \
+                       '<td align="center">' + str(item[CLUB_ID]) + '</td>' + \
+                       '<td align="center">' + str(item[ORGANIZATION_NAME]) + '</td>' + \
+                       '<td align="center">' + str(item[NUMBER_OF_MEMBERS]) + '</td>' + \
+                       '<td align="middle">' + str(item[NUMBER_OF_REVIEWS]) + '</td>' + \
+                       '<td align="middle">' + yesORno(club[PAYMENT_REQUIRED]) + '</td>' + \
+                       '<td align="middle">' + str(item[MEMBERSHIP_COST]) + '</td>' + '</tr>'
+
+        key += 1
+
+    return (header + message_out + footer)
+
+
+
+
 #-------------------- Home Page Handler --------------------
 @my_app.route('/')
 @my_app.route('/home')
@@ -244,18 +377,24 @@ def homePage():
     #SJSU_link2 {        
         float:left;
         position: relative;
-        top: 47px;
+        top: 38px;
         left: -90px;
-    }    
-    #SJSU_link3 {
+    }
+     #SJSU_link3 {
         float:left;
         position: relative;
-        top: 80px;
-        left: -242px;
+        top: 61px;
+        left: -245px;
+    }
+    #SJSU_link4 {
+        float:left;
+        position: relative;
+        top: 83px;
+        left: -429px;
     }
     
     #title {
-        padding-right: 320px;
+        padding-right: 540px;
         padding-top: 10px;
         margin: 0;
     }
@@ -312,16 +451,19 @@ def homePage():
             <a href="http://www.sjsu.edu/"> SJSU Website </a> 
         </div>
         <div id="SJSU_link2">
-            <a href="http://127.0.0.1:5000/showall"> List of all organizations</a>
+            <a href="http://127.0.0.1:5000/showall"> List of all Organizations</a>
         </div>
         <div id="SJSU_link3">
+            <a href="http://127.0.0.1:5000/details"> Further Organization Details</a>
+        </div>
+        <div id="SJSU_link4">
             <a href="http://www.sjsu.edu/getinvolved/studentorgs/new/"> Get Involved </a>
         </div>
     
-        <h1 id="title"> San Jose State University </h1>
+        <h1 id="title"> San Jose State </h1>
         <h1 id="title2"> Clubs & Organizations </h1>
         <h3> Welcome to the San Jose State University's Organization Directory</h3>
-        <p> A simple and hasstle-free place to find the organization that suits you! </p>
+        <p> A simple and hassle-free way to find organizations that interest you! </p>
         <div id="logo2div"> 
             <img src="http://www.books-not-bombs.com/content/images/schools/sjsu.png" id="logo2">
         </div>
@@ -349,13 +491,28 @@ def homePage():
 @my_app.route('/showall')
 
 def organizations():
-    command = """SELECT {a}.club_id, {a}.organization_name, {a}.president, {a}.location, {b}.category, {a}.rating, {a}.number_of_reviews, {a}.payment_required, {a}.membership_cost
+    command = """SELECT {a}.club_id, {a}.organization_name, {a}.president, {a}.location, {b}.category, {a}.rating
                       FROM {a} join {b} ON {a}.category_id = {b}.category_id
         """.format(a="organizations", b='category')
     cursor.execute(command)
     club_data = cursor.fetchall()
 
-    return (print_table(club_data))
+    return (print_maintable(club_data))
+
+
+#-------------------- Further Details Handler --------------------
+@my_app.route('/details')
+
+def details():
+    command = """SELECT {c}.club_id, {a}.organization_name, {c}.number_of_members, {c}.number_of_reviews, {c}.payment_required, {c}.membership_cost
+                          FROM {c} join {a} ON {c}.club_id = {a}.club_id
+            """.format(a="organizations", c='details')
+    cursor.execute(command)
+    club_data = cursor.fetchall()
+
+    return (print_subtable(club_data))
+
+
 
 
 
@@ -368,15 +525,18 @@ def get_message(key):
     PRESIDENT = 1
     LOCATION = 2
     RATING = 3
-    NUMBER_OF_REVIEWS = 4
+    NUMBER_OF_MEMBERS = 4
     PAYMENT_REQUIRED = 5
     MEMBERSHIP_COST = 6
-    CATEGORY = 7
+    URL = 7
+    DESCRIPTION = 8
+    CLUB_ID = 9
   
     
-    command = """ SELECT {a}.Organization_name, {a}.President, {a}.Location, {a}.Rating, {a}.number_of_reviews, {a}.payment_required,{a}.membership_cost,{a}.description,{a}.Image_URL,{b}.category
-                         FROM {a} join {b} ON {a}.category_ID = {b}.category_ID
-    """.format(a="Organizations", b='category', p1=key) 
+    command = """ SELECT {a}.club_id, {a}.Organization_name, {a}.description, {a}.President, {a}.Location, {a}.Rating, {c}.number_of_members, {c}.payment_required,{c}.membership_cost,{c}.Image_URL
+                         FROM {a} join {c} ON {a}.club_id = {c}.club_id
+                         WHERE {a}.club_id = {p1}
+    """.format(a="Organizations", c='details', p1=key)
     cursor.execute(command)
     club_data = cursor.fetchall()                           
   
@@ -488,17 +648,17 @@ def get_message(key):
         </body> """
 
 
-
-
     message = '<table><col width="315">'
+    message += '<td><img src="'+str(club)[URL]+'style="width:220px;height:326px" id="logo2"></td>'
     message += '<p class="right"><b>Organization Name:</b>'+str(club[ORGANIZATION_NAME])+'</p>'
-    message += '<p class="right"><b>Classification:</b>   ' +str(club[CATEGORY])+ '</p>'
+    message += '<p class="right"><b>ID:</b>' + str(club[CLUB_ID]) + '</p>'
+    message += '<p class="right"><b>Description:</b>'+str(club[DESCRIPTION])+'</p>'
     message += '<p class="right"><b>Location:</b>   '+str(club[LOCATION])+ '</p>'
     message += '<p class="right"><b>President:</b>   '+str(club[PRESIDENT])+'</p>'
     message += '<p class="right"><b>Membership Fee:</b>   $'+str(club[MEMBERSHIP_COST])+ '</p>'
     message += '<p class="right"><b>Fee required to join?</b>   '+yesORno(club[PAYMENT_REQUIRED])+ '</p>'
     message += '<p class="right"><b>Rating:</b>   '+str(club[RATING])+ '</p>'
-    message += '<p class="right" id="bottom"><b>Number of Reviews</b>   '+str(club[NUMBER_OF_REVIEWS])+ '</p></td>'
+    message += '<p class="right" id="bottom"><b>Number of Reviews:</b>   '+str(club[NUMBER_OF_MEMBERS])+ '</p></td>'
 
 
     return header2+message+footer2
@@ -556,21 +716,22 @@ def club_search ():
         if validation !="":
             validation += " AND "
         validation += "organizations.membership_cost= "+str(membership_cost)
-        
+
+        #MARTIN MAKE SURE CHANGE THE B'S TO C'S ON YOUR VIEWS FILE
     if validation == "":
-        command = """ SELECT {a}.club_id, {a}.president, {a}.location, {a}.rating, {a}.number_of_reviews, {a}.payment_required, {a}.membership_cost
-        ON {a} join {b} FROM {a}.category_ID = {b}.category_ID
+        command = """ SELECT {a}.club_id, {a}.president, {a}.location, {a}.rating, {c}.number_of_reviews, {c}.payment_required, {c}.membership_cost
+        ON {a} join {c} FROM {a}.club_id = {c}.club_ID
         WHERE {val}
-        """.format(a="organizations",b= 'category')
+        """.format(a="organizations",c= 'details')
     else:
-        command = """ SELECT {a}.club_id, {a}.president, {a}.location, {a}.rating, {a}.number_of_reviews, {a}.payment_required, {a}.membership_cost
-        ON {a} join {b} FROM {a}.category_ID = {b}.category_ID
+        command = """ SELECT {a}.club_id, {a}.president, {a}.location, {a}.rating, {c}.number_of_reviews, {c}.payment_required, {c}.membership_cost
+        ON {a} join {c} FROM {a}.category_ID = {c}.club_ID
         WHERE {val}
-        """.format(a="organizations",b= 'category',val = validation)
+        """.format(a="organizations",c= 'details',val = validation)
     
     cursor.execute(command)
     club_data = cursor.fetchall()
-    return (print_table(club_data))
+    return (print_maintable(club_data))
 
 '''We have to create new def function for this because the print_table is set for the home page only'''
 
@@ -679,13 +840,13 @@ def show_review():
         </div>
         <br>
     </div>
-    <p class="thick"> Guidelines for writing a review </p>
+    <p class="thick"> Guidelines for writing a review: </p>
     <pre>         - Why did you join?
-         - What was the benefit?
-         - Was the club members friendly?
-         - How is the club?
+         - What did you get out of the club?
+         - Were the club members friendly/helpful?
+         - What was your overall impression of the club?
     </pre>
-    <p class="outset"> Please tell us your thoughts about the club </p>
+    <p class="outset"> Please tell us your thoughts: </p>
     <div>
         <form action="/action_page.php">
             <label for="orgname">Organization Name</label>
