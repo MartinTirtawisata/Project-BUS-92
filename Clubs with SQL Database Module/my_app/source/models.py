74e2a5371cb250da9d03f2ac5b8622f3d0c6b505
Carlos Quirarte <cQuirarteJr@gmail.com>
2017-05-05 15:58:50 -0700

    latest version

--- Clubs with SQL Database Module/my_app/source/models.py
+++ Clubs with SQL Database Module/my_app/source/models.py
@@ -1,7 +1,3 @@
-from wtforms import Form, TextField, DecimalField, SelectField
-from wtforms.validators import InputRequired, NumberRange
-from decimal import Decimal
-
 import sqlite3
 sqlite_file = 'SJSU_Organizations.sqlite'
 
@@ -9,14 +5,12 @@ conn = sqlite3.connect(sqlite_file)
 cursor = conn.cursor()
 
 
-class ReviewForm(Form): #This is used in views.py
-    first_name = TextField(
-            label="First Name",
-            validators=[InputRequired()])
-    last_name = TextField(
-        label="Last Name",
-        validators=[InputRequired()])
 
-    user_review = TextField(
-            label='User Review',
-            validators=[InputRequired()])
+'''
+def insert_user_review(orgName,userReview,numReview):
+    con = sql.connect ("database.db")
+    cur = con.cursor()
+    cur.execute("INSERT INTO user_review (orgName, userReview,numReview)")
+    con.commit()
+    con.close()
+'''
