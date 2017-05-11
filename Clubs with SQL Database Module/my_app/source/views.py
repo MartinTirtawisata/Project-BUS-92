52cc892f9f3128924fc35ac25179e7d72fe943f5
MartinTirtawisata <martin.tirtawisata@sjsu.edu>
2017-05-10 23:41:35 -0700

    Add files via upload

--- Clubs with SQL Database Module/my_app/source/views.py
+++ Clubs with SQL Database Module/my_app/source/views.py
@@ -5,100 +5,6 @@ my_app = Blueprint('app', __name__)
 
 from my_app.source.models import ReviewForm
 
-
-#-------------Function for printing all Organizations (Main Table)---------
-
-def print_maintable(SJSU_Organizations):
-    CLUB_ID = 0
-    ORGANIZATION_NAME = 1
-    PRESIDENT = 2
-    NUMBER_OF_MEMBERS = 3
-    CATEGORY = 4
-    RATING = 5
-
-
-    footer = """
-    </table></body>
-    """
-    message_out = ''
-
-    key = 0
-    for item in SJSU_Organizations:
-        message_out += '<tr>'+\
-                       '<td align="center">'+str(item[CLUB_ID])+'</td>'+ \
-                       '<td align="center">''<a class="two" href="http://127.0.0.1:5000/show/' + str(item[CLUB_ID]) + '">' + str(item[ORGANIZATION_NAME]) + '</a></td>' + \
-                       '<td align="center">'+str(item[PRESIDENT])+'</td>'+\
-                       '<td align="middle">'+str(item[NUMBER_OF_MEMBERS])+'</td>'+\
-                       '<td align="middle">'+str(item[CATEGORY])+'</td>'+\
-                       '<td align="middle">'+str(item[RATING])+'</td>'+'</tr>'
-
-        key += 1
-    
-    return (render_template("main_table.html")+message_out+footer)
-
-
-
-#----------Function to change boolean to yes or no in sub-table---------
-
-def yesORno(boolean):
-    if (boolean == "TRUE"):
-        return "Yes"
-    elif (boolean == "FALSE"):
-        return "No"
-    
-
-#----------Funciton to print sub-table ------------------------------
-
-def print_subtable(SJSU_Organizations):
-    CLUB_ID = 0
-    ORGANIZATION_NAME = 1
-    NUMBER_OF_MEMBERS = 2
-    NUMBER_OF_REVIEWS = 3
-    PAYMENT_REQUIRED = 4
-    MEMBERSHIP_COST = 5
-
-    
-
-    footer = """
-    </table></body>
-    """
-    message_out = ''
-
-    key = 0
-    for item in SJSU_Organizations:
-        message_out += '<tr>' + \
-                       '<td align="center">' + str(item[CLUB_ID]) + '</td>' + \
-                       '<td align="center">''<a class="two" href="http://127.0.0.1:5000/show/' + str(item[CLUB_ID]) + '">' + str(item[ORGANIZATION_NAME]) + '</a></td>' + \
-                       '<td align="center">' + str(item[NUMBER_OF_MEMBERS]) + '</td>' + \
-                       '<td align="middle">' + str(item[NUMBER_OF_REVIEWS]) + '</td>' + \
-                       '<td align="middle">' + yesORno(str(item[PAYMENT_REQUIRED])) + '</td>' + \
-                       '<td align="middle">' + str(item[MEMBERSHIP_COST]) + '</td>' + '</tr>'
-
-        key += 1
-        
-
-    return (render_template("sub_table.html") + message_out + footer)
-
-
-#---------------------- Function to print categories table  -------------------
-
-def print_categories(Cat_Data):
-    CAT_ID = 0
-    CATEGORY = 1
-    
-    footer = """
-    </table></body>
-    """
-    message_out = ''
-    
-    for item in Cat_Data:
-        message_out += '<tr>' + \
-                        '<td align="center">' + str(item[CAT_ID]) + '</td>' + \
-                        '<td align="center">''<a class="three" href="http://127.0.0.1:5000/category/' + str(item[CAT_ID]) + '">' + str(item[CATEGORY]) + '</a></td>' + '</tr>'
-    
-    return (render_template("categories.html") + message_out + footer)
-
-
 #-------------------- Home Page Handler --------------------
 
 @my_app.route('/')
@@ -117,7 +23,7 @@ def organizations():
     cursor.execute(command)
     club_data = cursor.fetchall()
 
-    return (print_maintable(club_data))
+    return render_template("main_table.html",club_list=club_data)
  
 #-------------------- Further Details Handler --------------------
 
@@ -130,67 +36,34 @@ def details():
     cursor.execute(command)
     club_data2 = cursor.fetchall()
 
-    return (print_subtable(club_data2))
+    def yesORno(boolean):
+        if (boolean == "TRUE"):
+            return "Yes"
+        elif (boolean == "FALSE"):
+            return "No"
 
-
-
+    return render_template("sub_table.html", sub_list = club_data2)
 
 #-------------------- Show Key Handler --------------------
 
 #Parameters: Key, integer
 @my_app.route('/show/<key>') #Ways to control the parameter
 
-def get_message(key):    
-    ORGANIZATION_NAME = 0
-    CLUB_ID = 1
-    DESCRIPTION = 2
-    LOCATION = 3
-    PRESIDENT = 4
-    MEMBERSHIP_COST = 5
-    PAYMENT_REQUIRED = 6
-    RATING = 7
-    NUMBER_OF_MEMBERS = 8
-    URL = 9
-    
-    
+def get_message(key):
+
     command = """ SELECT {a}.Organization_name, {a}.club_id, {a}.description, {c}.Location, {a}.President, 
                          {c}.membership_cost, {c}.payment_required, {a}.rating, {a}.number_of_members, {c}.Image_URL
                          FROM {a} join {c} ON {a}.club_id = {c}.club_id
-    """.format(a="Organizations", c='details')
+                         WHERE {a}.club_id = {p1}
+    """.format(a="Organizations", c='details', p1=key)
     
     cursor.execute(command)
-    club_data3 = cursor.fetchall()                           
-    club = club_data3
+    club_data3 = cursor.fetchall()
+    if len(club_data3) == 0:
+        return "Page Error. The key " + key + " cannot be found"
+    individual_club = club_data3[0]
 
-    index = int(key)
-
-    if (len(club) == 0 or key == "0"):
-        return "The key "+ key + " was not found"
-    if index > 160:
-        return "The key "+ key + " was not found"
-
-        
-  
-    
-    total = int(key)
-    INDEX = (total - 1)
-    
-    
-    message = '<table><col width="315">'
-    message += '<td><img src="'+str(club[INDEX][URL])+'"style="width:280px;height:320px" id="logo2"></td>'
-    message += '<td><p class="right"><b>Organization Name: </b>'+str(club[INDEX][ORGANIZATION_NAME])+'</p>'
-    message += '<p class="right"><b>ID: </b>'+str(club[INDEX][CLUB_ID])+'</p>'
-    message += '<p class="right"><b>Description: </b>'+str(club[INDEX][DESCRIPTION])+'</p>'
-    message += '<p class="right"><b>Location: </b>   '+str(club[INDEX][LOCATION])+ '</p>'
-    message += '<p class="right"><b>President: </b>   '+str(club[INDEX][PRESIDENT])+'</p>'
-    message += '<p class="right"><b>Membership Fee: </b>   $'+str(club[INDEX][MEMBERSHIP_COST])+ '</p>'
-    message += '<p class="right"><b>Fee required to join? </b>   '+yesORno(club[INDEX][PAYMENT_REQUIRED])+ '</p>'
-    message += '<p class="right"><b>Rating: </b>   '+str(club[INDEX][RATING])+'</p>'
-    message += '<p class="right"><b>Number Of Members: </b>   '+str(club[INDEX][NUMBER_OF_MEMBERS])+'</p></td>'
-    
-    footer3 = """ </table> </body> """
-                                                        
-    return (render_template("show_one.html")+message+footer3)
+    return render_template("show_one.html",one_club = individual_club)
 
 
 
@@ -205,7 +78,7 @@ def show_categories():
     cursor.execute(command)
     club_data = cursor.fetchall()
 
-    return print_categories(club_data)
+    return render_template("categories.html", club_category = club_data)
 
 
 #------------------ Individual Category Pages Handler ----------------
@@ -222,7 +95,7 @@ def one_category(key):
     cursor.execute(command)
     club_data = cursor.fetchall()
 
-    return (print_maintable(club_data))
+    return render_template("main_table.html", club_list = club_data)
     
     
 
@@ -297,7 +170,7 @@ def club_search ():
     
     cursor.execute(command)
     club_data = cursor.fetchall()
-    return (print_maintable(club_data))
+    return render_template("main_table.html",club_list=club_data)
 
 
         
